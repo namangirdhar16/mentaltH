faea09d70ec2b379278cd9f9ecb62fcf49d2cd71
namangirdhar16 <64246118+namangirdhar16@users.noreply.github.com>
2021-01-16 10:07:25 +0530

    start with socket.io

--- app.js
+++ app.js
@@ -2,6 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.listen(process.env.port || 3000, ()=>{
-    console.log('server is up and running on port 3000');
+const http = require('http');
+const { Z_NEED_DICT } = require('zlib');
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection',(socket)=>{
+    console.log('a new user is connnected');
+    socket.on('disconnect',()=>{
+        console.log('user has disconnected!');
+    })
 })
+
+
+
+app.get('/',(req,res)=>{
+   res.sendFile(__dirname + '/public/index.html');
+})
+
+
+
+
+
+
+
+
+
+server.listen(3000,()=>{
+    console.log('server is up and running on port 3000')
+})
+
+
+
